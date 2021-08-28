cd258fe9564b98236f1c0cd21f9daed3c9ea207c
marcopeg <marco.pegoraro@gmail.com>
2021-08-28 14:27:55 +0200

    Queue cache updates

--- services/products/src/feature-cache-monitor/index.js
+++ services/products/src/feature-cache-monitor/index.js
@@ -1,54 +1,56 @@
-const updateProducts = require("./update-products");
-const updateCounters = require("./update-counters");
-
 // Figure out which source of truth needs to be updated
-const changeType = (prev, curr) => {
+const getQueueName = (prev, curr) => {
   const changes = {};
-  if (prev.title !== curr.title) changes["sot_products"] = true;
-  if (prev.price !== curr.price) changes["sot_products"] = true;
-  if (prev.qt_available !== curr.qt_available) changes["sot_counters"] = true;
-  if (prev.qt_booked !== curr.qt_booked) changes["sot_counters"] = true;
+  if (prev.title !== curr.title) changes["update_sot_products"] = true;
+  if (prev.price !== curr.price) changes["update_sot_products"] = true;
+  if (prev.qt_available !== curr.qt_available)
+    changes["update_sot_counters"] = true;
+  if (prev.qt_booked !== curr.qt_booked) changes["update_sot_counters"] = true;
   return Object.keys(changes);
 };
 
-// Updater
-const getUpdateFn = (sot) => {
-  if (sot === "sot_products") return updateProducts;
-  if (sot === "sot_counters") return updateCounters;
+const featureCacheMonitor = ({ registerAction }) => {
+  registerAction({
+    hook: "$FASTIFY_POST",
+    handler: {
+      url: "/on-cache-products-update",
+      handler: async (req) => {
+        const { op, data } = req.body.event;
 
-  return () => {
-    console.log("Unhandled SOT:", sot);
-  };
-};
+        if (op === "UPDATE") {
+          // Skip updates from SOT
+          if (data.new.etag_sot !== data.old.etag_sot) {
+            return "skipped";
+          }
 
-const featureCacheMonitor = () => ({
-  hook: "$FASTIFY_POST",
-  handler: {
-    url: "/on-cache-products-update",
-    handler: async (req) => {
-      const { op, data } = req.body.event;
-
-      if (op === "UPDATE") {
-        // Skip updates from SOT
-        if (data.new.etag_sot !== data.old.etag_sot) {
-          return "skipped";
+          // Forward the event to each relevant queue for updating the
+          // related source of truth service:
+          for (const queue of getQueueName(data.old, data.new)) {
+            console.log("Queue update into:", queue);
+            await req.fetchq.doc.append(queue, req.body);
+          }
+          return "udated";
         }
 
-        for (const sot of changeType(data.old, data.new)) {
-          getUpdateFn(sot)();
+        // @TODO
+        if (op === "DELETE") {
+          console.log("ON CACHE DELETE");
+          return "deleted";
         }
-        return "udated";
-      }
 
-      // @TODO
-      if (op === "DELETE") {
-        console.log("ON CACHE DELETE");
-        return "deleted";
+        return "unhandled";
       }
-
-      return "unhandled";
     }
-  }
-});
+  });
+
+  // Upsert the queues
+  registerAction({
+    hook: "$FETCHQ_REGISTER_QUEUE",
+    handler: () => [
+      { name: "update_sot_products" },
+      { name: "update_sot_counters" }
+    ]
+  });
+};
 
 module.exports = featureCacheMonitor;
