e4df5aca749fa883db8b82e8082e73d33fe5e320
marcopeg <marco.pegoraro@gmail.com>
2021-09-01 20:53:08 +0200

    Handle products delete in the counters service

--- services/sot_counters/src/handle-changelog/on-product-was-deleted.js
+++ services/sot_counters/src/handle-changelog/on-product-was-deleted.js
@@ -1,5 +1,16 @@
-const onProductWasDeleted = (log, { db, logEvent }) => {
-  console.log("@onProductWasDeleted not implemented");
+const onProductWasDeleted = async (log, { db, logEvent }) => {
+  // Remove the local references to the counters
+  const res = await db.query(
+    `
+    DELETE FROM "public"."sot_counters"
+    WHERE "product_id" = $1
+    RETURNING *
+  `,
+    [log.new_data.id]
+  );
+
+  // Notify the creation of the counters
+  await logEvent("counters-was-deleted", res.rows);
 };
 
 module.exports = onProductWasDeleted;
